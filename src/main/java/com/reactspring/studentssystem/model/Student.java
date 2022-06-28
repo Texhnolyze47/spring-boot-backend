1a52f8a4d62ee6dd1d8831ec1c5c2605905f3492
Texhnolyze <ichigobankai136@gmail.com>
2022-06-27 19:20:46 -0500

    add: model student

--- src/main/java/com/reactspring/studentssystem/model/Student.java
+++ src/main/java/com/reactspring/studentssystem/model/Student.java
@@ -1,4 +1,49 @@
 package com.reactspring.studentssystem.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String address;
+
+    public Student() {}
+
+    public int getId() {  return id;  }
+
+    public boolean setId(int id) {
+        if (id >= 0){
+            this.id = id;
+            return true;
+         } else
+             return false;
+
+    }
+
+    public String getName() {  return name;  }
+
+    public boolean setName(String name) {
+        if(!name.isEmpty()){
+            this.name = name;
+            return true;
+        } else
+            return false;
+    }
+
+    public String getAddress() {  return address;  }
+
+    public boolean setAddress(String address) {
+        if(!address.isEmpty()){
+            this.address = address;
+            return true;
+        } else
+            return false;
+    }
 }
--- src/main/java/com/reactspring/studentssystem/repository/StudentRepository.java
+++ src/main/java/com/reactspring/studentssystem/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.reactspring.studentssystem.repository;
 
-public interface StudentRepository {
+import com.reactspring.studentssystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer > {
 }

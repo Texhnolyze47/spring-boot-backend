73b8848c11fcb684c487b7041eb7efac25926831
Texhnolyze <ichigobankai136@gmail.com>
2022-06-28 19:00:23 -0500

    add: conexion con  la base de datos

--- src/main/java/com/reactspring/studentssystem/StudentssystemApplication.java
+++ src/main/java/com/reactspring/studentssystem/StudentssystemApplication.java
@@ -2,6 +2,9 @@ package com.reactspring.studentssystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class StudentssystemApplication {
@@ -9,5 +12,6 @@ public class StudentssystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentssystemApplication.class, args);
 	}
+
 
 }

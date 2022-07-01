93678ad3b3da916866b2539ec0ebaf161d0b5e47
Texhnolyze <ichigobankai136@gmail.com>
2022-07-01 17:41:19 -0500

    add: student config

--- src/main/java/com/reactspring/studentssystem/model/StudentConfig.java
+++ src/main/java/com/reactspring/studentssystem/model/StudentConfig.java
@@ -1,4 +1,37 @@
 package com.reactspring.studentssystem.model;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.*;
+
+@Configuration
 public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student ivan = new Student(
+                    "Ivan",
+                    "ivanr@gmail.com",
+                    LocalDate.of(2001, DECEMBER, 14),
+                    20
+            );
+            Student Erick = new Student(
+                    "Erick",
+                    "Erickr@gmail.com",
+                    LocalDate.of(2002, JANUARY, 14),
+                    20
+            );
+
+            repository.saveAll(
+                    List.of(ivan, Erick)
+            );
+        };
+    }
+
 }

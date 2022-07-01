60e6fe8c98f9d38bcd4e2a9db33e4b31e5543524
Texhnolyze <ichigobankai136@gmail.com>
2022-07-01 16:25:18 -0500

    add: api layer

--- src/main/java/com/reactspring/studentssystem/StudentssystemApplication.java
+++ src/main/java/com/reactspring/studentssystem/StudentssystemApplication.java
@@ -4,7 +4,6 @@ import com.reactspring.studentssystem.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -12,24 +11,11 @@ import java.util.List;
 
 
 @SpringBootApplication
-@RestController
 public class StudentssystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentssystemApplication.class, args);
 	}
-	@GetMapping
-	public List<Student> hello(){
-		return List.of(
-				new Student(
-						1L,
-						"Ivan",
-						"ivan.r@gmail.com",
-						LocalDate.of(2001, Month.DECEMBER,14),
-						20
-						)
-		);
 
-	}
 
 }

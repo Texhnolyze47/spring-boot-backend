3c8b44c81fbc2309a9b497d077671735fb91ae48
Texhnolyze <ichigobankai136@gmail.com>
2022-07-01 20:12:32 -0500

    add: borrar y actuatilzar usuario

--- src/main/java/com/reactspring/studentssystem/model/StudentRepository.java
+++ src/main/java/com/reactspring/studentssystem/model/StudentRepository.java
@@ -1,10 +1,19 @@
 package com.reactspring.studentssystem.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 //La anotacion @Repository indica que esta interface
 //es la responsable en acceder en el acceso a los datos
 @Repository
 //aqui tenemos que especificar el tipo de objecto y el tipo que tiene el ID
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentsByEmail(String email);
 }
+
+
+

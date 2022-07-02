297cca487089f0e7a82357df8cc081a1bc439cb7
Texhnolyze <ichigobankai136@gmail.com>
2022-07-02 10:39:04 -0500

    se desactivo config student

--- src/main/java/com/backendspring/studentssystem/model/StudentConfig.java
+++ src/main/java/com/backendspring/studentssystem/model/StudentConfig.java
@@ -11,27 +11,27 @@ import static java.time.Month.*;
 
 @Configuration
 public class StudentConfig {
-
-    @Bean
-    CommandLineRunner commandLineRunner(StudentRepository repository){
-        return args -> {
-            Student ivan = new Student(
-                    "Ivan",
-                    "ivanr@gmail.com",
-                    LocalDate.of(2001, DECEMBER, 8)
-
-            );
-            Student Erick = new Student(
-                    "Erick",
-                    "Erickr@gmail.com",
-                    LocalDate.of(2008, JANUARY, 4)
-
-            );
-
-            repository.saveAll(
-                    List.of(ivan, Erick)
-            );
-        };
-    }
+//
+//    @Bean
+//    CommandLineRunner commandLineRunner(StudentRepository repository){
+//        return args -> {
+//            Student ivan = new Student(
+//                    "Ivan",
+//                    "ivanr@gmail.com",
+//                    LocalDate.of(2001, DECEMBER, 8)
+//
+//            );
+//            Student Erick = new Student(
+//                    "Erick",
+//                    "Erickr@gmail.com",
+//                    LocalDate.of(2008, JANUARY, 4)
+//
+//            );
+//
+//            repository.saveAll(
+//                    List.of(ivan, Erick)
+//            );
+//        };
+//    }
 
 }
